04b672030aed207f3519941679e2eee007520c4a
DANR1BEIRO <danribeirodev@gmail.com>
2025-12-02 12:54:00 -0300

    refactor(Main): Agora o Main passa a utilizar o RobotService para as regras de negócio.

--- src/com/daniel/robotics/app/Main.java
+++ src/com/daniel/robotics/app/Main.java
@@ -1,4 +1,14 @@
 package com.daniel.robotics.app;
 
+import com.daniel.robotics.domain.Mech;
+import com.daniel.robotics.enums.RobotType;
+import com.daniel.robotics.service.RobotService;
+
 public class Main {
+    public static void main(String[] args) {
+        Mech megaman = new Mech("Megaman", RobotType.COMBAT);
+
+        RobotService.performAttack(megaman);
+        RobotService.performAttack(megaman);
+    }
 }
