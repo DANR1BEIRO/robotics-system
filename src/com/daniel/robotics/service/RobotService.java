19633a810c28927ee38ca1b3b37448d87a34e4fb
DANR1BEIRO <danribeirodev@gmail.com>
2025-12-02 12:52:53 -0300

    feat(Service): Implementa a classe RobotService que ficará responsável pel as regras de negócio. Método adicionado: performAttack com generics restrito a tipos que estendem Robot e implementam Combat para garantir acesso seguro a attack() e getName() sem casts.

--- src/com/daniel/robotics/service/RobotService.java
+++ src/com/daniel/robotics/service/RobotService.java
@@ -1,4 +1,17 @@
 package com.daniel.robotics.service;
 
-public class RobotService {
+import com.daniel.robotics.domain.Combat;
+
+import com.daniel.robotics.domain.Robot;
+import com.daniel.robotics.exceptions.LowBatteryException;
+
+public final class RobotService {
+
+    public static <T extends Robot & Combat> void performAttack(T robot) {
+        try {
+            robot.attack();
+        } catch (LowBatteryException e) {
+            System.out.println(robot.getName() + " não tem energia suficiente para atacar.\n" + e.getMessage());
+        }
+    }
 }
