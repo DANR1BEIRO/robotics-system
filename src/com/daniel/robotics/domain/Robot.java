ef5e218b137a9090ef1d20c022d473bfa442e3d7
DANR1BEIRO <danribeirodev@gmail.com>
2025-12-02 12:44:11 -0300

    feat(Robot): Adiciona um getBattery e deixa de capturar a LowBatteryException no método consumeEnergy(), delegando essa tarefa para o RobotService.

--- src/com/daniel/robotics/domain/Robot.java
+++ src/com/daniel/robotics/domain/Robot.java
@@ -5,10 +5,26 @@ import com.daniel.robotics.enums.RobotType;
 public abstract class Robot {
     private String name;
     private RobotType robotType;
+    private Battery battery;
 
     public Robot(String name, RobotType robotType) {
         this.name = name;
         this.robotType = robotType;
+        this.battery = new Battery(robotType);
+
+    }
+
+    protected void consumeEnergy(int amount) {
+        battery.consume(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Robot{" +
+                "name='" + name + '\'' +
+                ", robotType=" + robotType +
+                ", battery=" + battery +
+                '}';
     }
 
     public String getName() {
@@ -19,11 +35,7 @@ public abstract class Robot {
         return robotType;
     }
 
-    @Override
-    public String toString() {
-        return "Robot{" +
-                "name='" + name + '\'' +
-                ", robotType=" + robotType +
-                '}';
+    public Battery getBattery() {
+        return battery;
     }
 }
