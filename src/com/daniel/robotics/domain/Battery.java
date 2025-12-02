f31e402908785c5e103bd85bba00f675aeeffb14
DANR1BEIRO <danribeirodev@gmail.com>
2025-12-02 10:50:55 -0300

    feat(enum): Cria o rich enum RobotType, com atributos para inicializar cada robô de forma padronizada com relacao à quantidade de bateria e os custos de cada operacao.
    feat(Battery): A classe Battery inicializa com base nas regras do enum RobotType.

--- src/com/daniel/robotics/domain/Battery.java
+++ src/com/daniel/robotics/domain/Battery.java
@@ -1,4 +1,22 @@
 package com.daniel.robotics.domain;
 
-public class Battery {
+import com.daniel.robotics.enums.RobotType;
+
+public abstract class Battery {
+    private int currentEnergy;
+    private int maxCharge;
+
+
+    public Battery(RobotType robotType) {
+        this.maxCharge = robotType.getMaxBattery();
+        this.currentEnergy = maxCharge;
+    }
+
+    public int getCurrentEnergy() {
+        return currentEnergy;
+    }
+
+    public int getMaxCharge() {
+        return maxCharge;
+    }
 }
--- src/com/daniel/robotics/enums/RobotType.java
+++ src/com/daniel/robotics/enums/RobotType.java
@@ -1,7 +1,39 @@
 package com.daniel.robotics.enums;
 
 public enum RobotType {
-    DRONE,
-    MECH,
-    ANDROID
+    DRONE(100, 20, 5, 0),
+    MECH(200, 10, 0, 0),
+    ANDROID(70, 0, 0, 5);
+
+    private int maxBattery;
+    private int attackCost;
+    private int flyCost;
+    private int workCost;
+
+    RobotType(int maxBattery, int attackCost, int flyCost, int workCost) {
+        this.maxBattery = maxBattery;
+        this.attackCost = attackCost;
+        this.flyCost = flyCost;
+        this.workCost = workCost;
+    }
+
+    public int getMaxBattery() {
+        return maxBattery;
+    }
+
+    public int getAttackCost() {
+        return attackCost;
+    }
+
+    public int getFlyCost() {
+        return flyCost;
+    }
+
+    public int getWorkCost() {
+        return workCost;
+    }
 }
+
+
+
+
