8ade7de62065dce84ae44bb570319d78ec8d0d86
DANR1BEIRO <danribeirodev@gmail.com>
2025-12-03 11:56:10 -0300

    test(main): Testa as novas funcionalidades de attack e fly.

--- src/com/daniel/robotics/app/Main.java
+++ src/com/daniel/robotics/app/Main.java
@@ -1,5 +1,7 @@
 package com.daniel.robotics.app;
 
+import com.daniel.robotics.domain.Android;
+import com.daniel.robotics.domain.Drone;
 import com.daniel.robotics.domain.Mech;
 import com.daniel.robotics.enums.RobotType;
 import com.daniel.robotics.service.RobotService;
@@ -7,14 +9,21 @@ import com.daniel.robotics.service.RobotService;
 public class Main {
     public static void main(String[] args) {
         Mech megaman = new Mech("Megaman", RobotType.COMBAT);
+        Android ns5 = new Android("NS-5", RobotType.ANDROID);
+        Drone eva = new Drone("EVA", RobotType.DRONE);
 
         RobotService.performRecharge(megaman, 200);
+        RobotService.performRecharge(eva, 10);
+        RobotService.performRecharge(ns5, 2);
 
         RobotService.performAttack(megaman);
-        RobotService.performAttack(megaman);
+        RobotService.performRecharge(megaman, 5);
 
-        RobotService.performRecharge(megaman, 10);
-        RobotService.performRecharge(megaman, 40);
+        RobotService.performAttack(eva);
+        RobotService.performRecharge(eva, 10);
+
+        RobotService.performWork(ns5);
+        RobotService.performRecharge(ns5, 2);
 
 
     }
