0abb5e8c43211b61c265b5496f3b8da1fce2ec81
DANR1BEIRO <danribeirodev@gmail.com>
2025-12-04 12:08:57 -0300

    refactor(service): Remove prints da camada de service, adiciona turnOn/turnOff ao domínio e centraliza mensagens na camada Robot.

--- src/com/daniel/robotics/service/RobotService.java
+++ src/com/daniel/robotics/service/RobotService.java
@@ -6,6 +6,7 @@ import com.daniel.robotics.domain.Flyable;
 import com.daniel.robotics.domain.Robot;
 import com.daniel.robotics.domain.Workable;
 import com.daniel.robotics.exceptions.LowBatteryException;
+import com.daniel.robotics.exceptions.RobotOfflineException;
 import com.daniel.robotics.exceptions.SystemOverloadException;
 
 public final class RobotService {
@@ -14,25 +15,28 @@ public final class RobotService {
         try {
             robot.attack();
         } catch (LowBatteryException e) {
-            System.out.println(robot.getName() + " não tem energia suficiente para atacar.\n" + e.getMessage());
+            System.out.println(e.getMessage());
+        } catch (RobotOfflineException e) {
+            System.out.println(e.getMessage());
         }
-        System.out.println();
     }
 
     public static void performRecharge(Robot robot, int amount) {
         try {
-            robot.rechargeBattery(amount, robot.getName());
+            robot.rechargeBattery(amount);
         } catch (SystemOverloadException e) {
             System.out.println(e.getMessage());
+        } catch (RobotOfflineException e) {
+            System.out.println(e.getMessage());
         }
-        System.out.println(robot.getBattery());
-        System.out.println();
     }
 
     public static <T extends Robot & Workable> void performWork(T robot) {
         try {
             robot.work();
         } catch (SystemOverloadException e) {
+            System.out.println(e.getMessage());
+        } catch (RobotOfflineException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -42,8 +46,16 @@ public final class RobotService {
             robot.fly();
         } catch (SystemOverloadException e) {
             System.out.println(e.getMessage());
+        } catch (RobotOfflineException e) {
+            System.out.println(e.getMessage());
         }
-        System.out.println(robot.getBattery());
-        System.out.println();
+    }
+
+    public static void turnRobotOn(Robot robot) {
+        robot.turnOn();
+    }
+
+    public static void turnRobotOff(Robot robot) {
+        robot.turnOff();
     }
 }
